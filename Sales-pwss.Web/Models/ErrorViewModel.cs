c44e951978f68954629b3846800a6ace0f7885f7
renatomgaspar8 <renatomgaspars@gmail.com>
2025-07-01 11:41:50 +0100

    Init web project

---
+++ Sales-pwss.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sales_pwss.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
